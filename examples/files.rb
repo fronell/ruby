72c80a1f195e054f08947a56d3864e8d5c3d6231
Chris Wornell <fro.888@gmail.com>
2014-05-19 02:02:22 -0500

    Fixed string interpolation of __method__ in files.rb

--- examples/files.rb
+++ examples/files.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby -w
 
 def file_open_no_block
-  puts "BEGIN: __method__"
+  puts "BEGIN: #{__method__}"
   puts "Output contents of file"
   puts '$0 = file name of the ruby script'
   file_to_open = $0
@@ -11,17 +11,17 @@ def file_open_no_block
   f.seek(0, IO::SEEK_SET)
   puts f.read
   f.close
-  puts "END: __method__"
+  puts "END: #{__method__}"
 end
 
 def file_open_with_block
-  puts "BEGIN: __method__"
+  puts "BEGIN: #{__method__}"
   puts "Block form of File.open which closes the handle at the end of the block"
   file_to_open = $0
   File.open(file_to_open) do |f|
     puts f.read
   end
-  puts "END: __method__"
+  puts "END: #{__method__}"
 end
 
 def manage_cwd
@@ -30,7 +30,7 @@ def manage_cwd
   puts "new cwd: #{Dir.pwd}" 
 end
 
-#file_open_no_block
+file_open_no_block
 #file_open_with_block
 #file_open_with_block
 #manage_cwd
