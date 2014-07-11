f30ea2ca9f32717a73a91688dfb65ab59e5f5ad9
Chris Wornell <fro.888@gmail.com>
2014-07-11 01:06:38 -0500

    cleaned up/updated time.rb

--- examples/time.rb
+++ examples/time.rb
@@ -38,6 +38,32 @@ sort of polluted the information of what you can do with vanilla Ruby since
 so many people are using it.
 =end
 
+=begin
+These directives can be used with strptime and strftime
+%a  The abbreviated weekday name (Sun).
+%A  The full weekday name (Sunday).
+%b  The abbreviated month name (Jan).
+%B  The full month name (January).
+%c  The preferred local date and time representation.
+%d  Day of the month (01 to 31).
+%H  Hour of the day, 24-hour clock (00 to 23).
+%I  Hour of the day, 12-hour clock (01 to 12).
+%j  Day of the year (001 to 366).
+%m  Month of the year (01 to 12).
+%M  Minute of the hour (00 to 59).
+%p  Meridian indicator (AM or PM).
+%S  Second of the minute (00 to 60).
+%U  Week number of the current year, starting with the first Sunday as the first day of the first week (00 to 53).
+%W  Week number of the current year, starting with the first Monday as the first day of the first week (00 to 53).
+%w  Day of the week (Sunday is 0, 0 to 6).
+%x  Preferred representation for the date alone, no time.
+%X  Preferred representation for the time alone, no date.
+%y  Year without a century (00 to 99).
+%Y  Year with century.
+%Z  Time zone name.
+%%  Literal % character.
+=end
+
 def time_class
   puts "BEGIN: #{__method__}"
   # There are two time classes:
@@ -87,60 +113,31 @@ def time_class
   puts "END: #{__method__}"
 end
 
-def datetime_class
-
-end
-
-def time_parsing
-=begin
-Time Formatting Directives:
-
-These directives in the following table are used with the method Time.strftime.
-Directive Description
-%a  The abbreviated weekday name (Sun).
-%A  The full weekday name (Sunday).
-%b  The abbreviated month name (Jan).
-%B  The full month name (January).
-%c  The preferred local date and time representation.
-%d  Day of the month (01 to 31).
-%H  Hour of the day, 24-hour clock (00 to 23).
-%I  Hour of the day, 12-hour clock (01 to 12).
-%j  Day of the year (001 to 366).
-%m  Month of the year (01 to 12).
-%M  Minute of the hour (00 to 59).
-%p  Meridian indicator (AM or PM).
-%S  Second of the minute (00 to 60).
-%U  Week number of the current year, starting with the first Sunday as the first day of the first week (00 to 53).
-%W  Week number of the current year, starting with the first Monday as the first day of the first week (00 to 53).
-%w  Day of the week (Sunday is 0, 0 to 6).
-%x  Preferred representation for the date alone, no time.
-%X  Preferred representation for the time alone, no date.
-%y  Year without a century (00 to 99).
-%Y  Year with century.
-%Z  Time zone name.
-%%  Literal % character.
-=end
-  
-  #Parsing may not always work, in the following, the parse method gets the month wrong
-  puts "Wrong: #{ DateTime.parse('02-09-2007 12:30:44 AM') }"
-  #Instead, we can specify the time format of the string to get the correct month
-  twelve_hour_clock_time = '%m-%d-%Y %I:%M:%S %p'
-  p = DateTime.strptime('02-09-2007 12:30:44 AM', twelve_hour_clock_time)
-  puts "Right: #{ p }"
-  puts Date.strptime('02-09-2007', '%m-%d-%Y')
-end
-
-######
-
-def date_method
+def date_class
+  puts "BEGIN: #{__method__}"
   #The Date class can be useful when you're only concernced with comparing dates 
   require 'date'
   now = Date.today
-  ninety_days_ago = (now - 90)
-  puts ninety_days_ago
-  d = Date.new(1997, 12, 31)
-  puts d
-  puts d + 10
+  puts "now=#{now}"
+  puts "90 days ago=#{(now - 90)}"
+  puts "Date.new=#{Date.new(1997, 12, 31)}"
+  puts "now.to_time=#{now.to_time}"
+  puts "now.to_datetime=#{now.to_datetime}"
+  puts "END: #{__method__}"
+end
+
+def datetime_class
+  puts "BEGIN: #{__method__}"
+  require 'date'
+  timestamp = "Sep  2 07:32:45 2014 GMT"
+  # DateTime.parse can parse this timestamp but Time can't
+  parsed = DateTime.parse(timestamp)
+  puts "Parsed [#{timestamp}]=#{parsed}"
+  puts "parsed.to_time=#{parsed.to_time}"
+  puts "parsed.to_date=#{parsed.to_date}"
+  puts "END: #{__method__}"
 end
 
 time_class
+date_class
+datetime_class
